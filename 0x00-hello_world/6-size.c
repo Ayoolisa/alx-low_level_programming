178ffaf9f0231bfec8683ae990e7c0afabb98430
Ayomide Kolawole Olisa <lekelisa247@gmail.com>
2022-09-08 13:51:06 -0700

    Modify 6-size.c file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main -Entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -9,9 +9,9 @@ int main(void)
 {
 	printf("Size of a char: %i bytes(s)\n", sizeof(char));
 	printf("Size of an int: %i byte(s)\n", sizeof(int));
-	printf("Size of along int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
 	return (0);
 }
 
