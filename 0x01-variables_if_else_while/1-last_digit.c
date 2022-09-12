03173ae7256d5e793f83deb76990881c53cbd97f
Ayomide Kolawole Olisa <lekelisa247@gmail.com>
2022-09-12 03:52:55 -0700

    Create 1-last_digit.c file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,16 +4,15 @@
 
 /**
  * main - Entry point
- * 
+ *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int n;
 
 	srand(time(0));
- 	n = rand() - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 
 	if (n % 10 > 5)
 	{
@@ -24,9 +23,8 @@ int main(void)
 		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	}
 	else if (n % 10 < 6)
-	{ 
+	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
-
 	return (0);
 }
