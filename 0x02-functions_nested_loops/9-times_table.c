2b56dca6061373cb8e2de06aa9d3f055bc2dd721
Ayomide Kolawole Olisa <lekelisa247@gmail.com>
2022-09-13 21:41:43 -0700

    Modify 9-times_table.c file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,24 +6,37 @@
  */
 void times_table(void)
 {
-	int x, y, product;
+	int x, y, z, u, d;
 
 	for (x = 0; x <= 9; x++)
 	{
-		_putchar(0);
-
 		for (y = 0; y <= 9; y++)
 		{
-			_putchar(',');
-			_putchar(' ');
+			z = x * y;
 
-			product = x * y;
+			if (z > 9)
+			{
+				u = z % 10;
+				d = (z - u) / 10;
 
-			if (product <= 9)
-				_putchar(' ');
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
+			}
 			else
-				_putchar((product / 10) + '0');
+			{
+				if (y != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+
+				_putchar(z + '0');
+			}
 		}
+
 		_putchar('\n');
 	}
 }
