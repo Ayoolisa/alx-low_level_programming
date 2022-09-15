b14a404dc9f160c5286dcad5407dba7de24fbb23
Ayomide Kolawole Olisa <lekelisa247@gmail.com>
2022-09-15 01:29:31 -0700

    Create 2-largest_number.c file

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,7 +14,7 @@ int largest_number(int a, int b, int c)
 
 	if (a >= b && b >= c)
 	{
-		largest = a;
+		largest = c;
 	}
 	else if (b >= a && b >= c)
 	{
@@ -22,7 +22,7 @@ int largest_number(int a, int b, int c)
 	}
 	else
 	{
-		largest = c;
+		largest = a;
 	}
 	return (largest);
 }
