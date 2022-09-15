5396ea087d519a1b98a9d02c0729ec42166e1d17
Ayomide Kolawole Olisa <lekelisa247@gmail.com>
2022-09-15 09:18:56 -0700

    Create 0-isupper.c file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _isupper(int c) - checks for uppercase character.
- * Return: 1 if c is uppercase
- * 	   0 otherwise
+ * _isupper - checks for uppercase character.
+ * @c: number being tested
+ * Return: 1 if c is uppercase 0 otherwise
  */
 
 int _isupper(int c)
