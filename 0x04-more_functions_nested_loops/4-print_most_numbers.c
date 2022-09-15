978a0bc01b2ab42e07a8d4f893bbd8ae41026a46
Ayomide Kolawole Olisa <lekelisa247@gmail.com>
2022-09-15 13:40:33 -0700

    Create 4-print_most_numbers.c file

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints the numbers, from 0 to 9, followed by a new line.
- * @c: number being tested
+ * print_most_numbers - prints the numbers, from 0 to 9.
  *
  * Return: Always 0
  */
 void print_most_numbers(void)
 {
-	int c;
-
-	c = 47;
+	int c = 47;
 
 	while (c < 57)
 	{
 		c++;
 		if (c != 50 && c != 52)
+		{
 			_putchar(c);
+		}
 	}
+
 	_putchar('\n');
 }
