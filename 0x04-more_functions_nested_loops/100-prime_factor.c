9bd901452a04c25a183518796c607c919b82b2f7
Ayomide Kolawole Olisa <lekelisa247@gmail.com>
2022-09-15 17:33:14 -0700

    Create 100-prime_factor.c file

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Finds and prints the largest prime
- * 	factor of the number 612852475143.
+ * main - Finds and prints the largest prime,
+ * factor of the number 612852475143.
  *
  * Return: Always 0.
  */
