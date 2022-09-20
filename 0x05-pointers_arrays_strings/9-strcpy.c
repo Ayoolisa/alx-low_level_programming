9868b7a9d2c6424f068b7249dfe8ea7c41992771
Ayomide Kolawole Olisa <lekelisa247@gmail.com>
2022-09-20 04:43:55 -0700

    Create 9-strcpy.c file

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by src
+ * to the buffer pointed to by dest.
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	
+	while(src[i])
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
