1f99b995555c8f7d8e2b83e8a01249c00ff27b9a
Ayomide Kolawole Olisa <lekelisa247@gmail.com>
2022-09-30 06:14:36 -0700

    Create 0-whatsmyname.c file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
- * whatsmyname - prints its name, followed by a new line.
+ * main - prints its name, followed by a new line.
+ * @argc: is the count of the arguments supplied to the program
+ * @argv: an array of pointer to char.
  *
  * Return: The name of the argument entered
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
