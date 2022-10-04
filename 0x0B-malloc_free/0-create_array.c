e12c4db7c5e9bc5bb1812cb1becbdef1d4272fc1
Ayomide Kolawole Olisa <lekelisa247@gmail.com>
2022-10-04 08:00:15 -0700

    Create 0-create_array.c fiel

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars, and initializes it with a specific char.
+ * @size: ths size of the data in bytes allocated in the memory
+* @c: A character to initialize the array.
+*
+* Return: a pointer to an array of characters.
+*/
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i = 0;
+	char *s;
+
+	s = malloc(100 * sizeof(char));
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	return (s);
+}
+
+
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,7 @@
+<h1>0x0B-malloc_free</h1>
+<h4>General Objectives</h4>
+<ul>
+<li>The use of Static and Dynamic Libraries</li>
+<li><i>Malloc</i> and <i>Free</i> and how to use them</li>
+<li>How to use <b>Valgrind</b> to check the memory leak.</li>
+</ul>
