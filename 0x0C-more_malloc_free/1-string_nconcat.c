9dc13b621a8d4965be2a4571a4731c66fecb909e
Ayomide Kolawole Olisa <lekelisa247@gmail.com>
2022-10-07 03:40:59 -0700

    Create 1-string_nconcat.c file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,6 +5,7 @@
  * an inputted number of bytes.
  * @s1: The first string.
  * @s2: The second string.
+ * @n: The maximum number of bytes of s2 to concatenate to s1.
  *
  * Return: If the function fails - NULL.
  * Otherwise - a pointer to the concatenated space in memory.
@@ -19,17 +20,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s1 = "";
 	}
-
 	if (s2 == NULL)
 	{
 		s2 = "";
 	}
-
 	for (i = 0; s1[i]; i++)
 	{
 		len++;
 	}
-
 	concat = malloc(sizeof(char) * (len + 1));
 
 	if (concat == NULL)
@@ -43,12 +41,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		concat[len++] = s1[i];
 	}
-
 	for (i = 0; s2[i] && i < n; i++)
 	{
 		concat[len++] = s2[i];
 	}
-
 	concat[len] = '\0';
 
 	return (concat);
