1f5c9e49345a89913427622edc00301d9dad6fcb
Ayomide Kolawole Olisa <lekelisa247@gmail.com>
2022-10-11 16:07:54 -0700

    Create 4-new_dog.c file

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,18 +19,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
-
 	while (name[len_name])
 		len_name++;
-
 	while (owner[len_owner])
 		len_owner++;
-
 	new_dog = malloc(sizeof(dog_t));
-
 	if (new_dog == NULL)
 		return (NULL);
-
 	cpyname = malloc(len_name + 1);
 	if (cpyname == NULL)
 		return (NULL);
@@ -40,7 +35,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		cpyname[i] = name[i];
 	}
 	cpyname[i] = '\0';
-
 	cpyowner = malloc(len_owner + 1);
 	if (cpyowner == NULL)
 		return (NULL);
@@ -50,7 +44,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		cpyowner[i] = owner[i];
 	}
 	cpyowner[i] = '\0';
-
 	new_dog->name = cpyname;
 	new_dog->age = age;
 	new_dog->owner = cpyowner;
