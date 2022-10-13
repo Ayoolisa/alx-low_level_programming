b47c8989f0bc096e1992c9c925595a8a73a06fb0
Ayomide Kolawole Olisa <lekelisa247@gmail.com>
2022-10-13 07:12:29 -0700

    Create (3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h) files

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -11,15 +11,10 @@
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {{"+", op_add}, {"-", op_sub}, {"*", op_mul}, {"/", op_div}, {"%", op_mod}, {NULL, NULL}};
-	int i;
+	int i = 0;
 
-    while (i < 5)
-    {
-	    if (*(ops[i]).op == *s && *(s + 1) == '\0')
-	    {
-		    return (ops[i].f);
-	    }
+    while (ops[i].op != NULL && *(ops[i].op) != *s)
 	    i++;
-    }
-    return (NULL);
+
+    return (ops[i].f);
 }
