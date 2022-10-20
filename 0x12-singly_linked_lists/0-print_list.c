dab94aad31348b5f339af83cd76452283f992e5b
Ayomide Kolawole Olisa <lekelisa247@gmail.com>
2022-10-20 14:48:36 -0700

    Create 0-print_list.c file

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,10 +2,12 @@
 
 /**
  * print_list -  a function that prints all the
- * elements of a list_t list. 
- * 
- * Return: The number of nodes.
+ * elements of a list_t list.
+ * @h: The list_t list.
+ *
+ * Return: The number of nodes in h.
  */
+
 size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
