4fd5a2132cc807f7dfc16574e0e5d4afd650d423
Ayomide Kolawole Olisa <lekelisa247@gmail.com>
2022-10-25 14:13:50 -0700

    Create listint_len.c file

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,15 +13,8 @@ size_t print_listint(const listint_t *h)
 
 	while (h)
 	{
-		if (h->n == '\0')
-		{
-			printf("[0] (nil)\n");
-		}
-		else
-		{
-			printf("%d\n", h->n);
-		}
 		nodes++;
+		printf("%d\n", h->n);
 		h = h->next;
 	}
 	return (nodes);
