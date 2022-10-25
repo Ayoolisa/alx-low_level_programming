c3c3eb167d428c4f04e89bcbc32755386930b163
Ayomide Kolawole Olisa <lekelisa247@gmail.com>
2022-10-25 13:44:15 -0700

    Create 0-print_listint.c file

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * listint_t - a function that prints all the elements
+ * of a listint_t list.
+ * @h: the list_t list.
+ *
+ * Return: the number of nodes in h.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		if (h->n == '\0')
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("%d\n", h->n);
+		}
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
